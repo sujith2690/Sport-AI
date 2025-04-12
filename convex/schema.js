901fca5c3f64eaa34b844d8b7b99770addd5bcee
Sujith P <sujithp2690@gmail.com>
2025-04-12 20:31:52 +0530

    database - convex added

---
+++ convex/schema.js
@@ -0,0 +1,12 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+    users: defineTable({
+        name: v.string(),
+        email: v.string(),
+        picture: v.string(),
+        // password: v.string(),
+        subscriptionId: v.optional(v.string()),
+    })
+})
